16398d10658149d1a4cbc69e5945acbc91dfa957
jambrizz <ambrizjovani@gmail.com>
2023-07-09 23:04:48 -0700

    set example problems version1

--- python_data_structure_tutorial/examples/example_sets.py
+++ python_data_structure_tutorial/examples/example_sets.py
@@ -13,7 +13,7 @@ def add_employee(set, employee):
 def remove_employee(set, employee):
     set.remove(employee)
     
-# Create a method to check if an employee is in the set
+# Create a method to check if an employee is in the set and return True if they are, False if they are not
 def found_employee(set, employee):
     if employee in set:
         return True
@@ -38,3 +38,37 @@ This part of the code is to show how to perform unions, intersections and differ
 You can copy and paste the code below to test out the code in your own environment.
 """
 
+# Create a method to perform a union between two sets
+def union(set1, set2):
+    newSet = set()
+    for x in set1:
+        newSet.add(x)
+    for x in set2:
+        newSet.add(x)
+    return newSet
+
+# Create a method to perform an intersection between two sets
+def intersection(set1, set2):
+    newSet = set()
+    for x in set1:
+        if x in set2:
+            newSet.add(x)
+    return newSet
+
+# Create a method to perform a difference between two sets
+def difference(set1, set2):
+    newSet = set()
+    for x in set1:
+        if x not in set2:
+            newSet.add(x)
+    for x in set2:
+        if x not in set1:
+            newSet.add(x)
+    return newSet
+
+employees_site_1 = set(["Mike", "Corey", "John", "Matt"])
+employees_site_2 = set(["Matt", "Luis", "John", "Corey"])
+
+print(union(employees_site_1, employees_site_2))
+print(intersection(employees_site_1, employees_site_2))
+print(difference(employees_site_1, employees_site_2))
